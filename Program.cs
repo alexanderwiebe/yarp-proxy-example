0345f349913babe5ab9d570a02c5ea36316a9d20
Alexander Wiebe <alexanderjwiebe@gmail.com>
2023-03-02 14:07:25 -0600

    adding proxy information

--- Program.cs
+++ Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/asdf", () => "{ \"title\": \"PROXY\" }");
 app.MapReverseProxy();
+
+// app.UseHttpsRedirection();
+// dotnet dev-certs https --trust
+// that's literally it
+
+app.UseStaticFiles();
+app.UseRouting();
+
 app.Run();
